1448672565081a872306dfba328614431cf6775d
np2complete <np_complete@hotmail.com>
2018-01-09 11:38:22 +0800

    log json request 01

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-  echo "hello i'm pupu / 180109"
+  //echo "hello i'm pupu / 180109"
+  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
+  $json_obj = json_decode($json_str); //轉json格式
+  $myfile = fopen("log.txt","w+") or die("unable to open file!");
+  fwrite($myfile, "\xEF\xBB\xBf".$json_str); //在字串前加入轉成utf8
+  fclose($myfile);
 ?>
